8d7014b73925c43088c552d15e852d53ee570bd5
Andy Schaul <andy.schaul@gmail.com>
2019-06-22 22:06:31 -0500

    add water pump test

---
+++ water_relay_test.py
@@ -0,0 +1,34 @@
+# Program to test relay for water pump
+
+# Wiring
+
+# Relay —> Pi
+# GND - GND
+# VCC - 5V
+# IN1 - Pin 16 (GPIO 23)
+
+
+import RPi.GPIO as GPIO
+import time
+
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+
+OutputPins = [23]
+
+for i in OutputPins:
+    GPIO.setup(i, GPIO.OUT)
+    GPIO.output(i, False)
+
+
+while True:
+    for i in OutputPins:
+        GPIO.output(i, True)
+        print('Relay On')
+    time.sleep(2)
+    
+    for i in OutputPins:
+        GPIO.output(i, False)
+        print('Relay Off')
+    time.sleep(2)
